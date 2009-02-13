c2eaa5fc87faaf63f94b4610e8d6afd4b46c8728
Marcos Alvares <mabj@levinas.(none)>
2009-02-13 18:24:39 -0300

    Modificando o script para geracao do ambiente para incluir a interface web no pacote gerado.

--- admin/create_enviroment.sh
+++ admin/create_enviroment.sh
@@ -20,10 +20,10 @@ cp -af ../stack/challenge_06.c ./ctf_ucon2/admin/code/vulndev/challenge_10.c
 cp -af ../stack/challenge_05.c ./ctf_ucon2/admin/code/vulndev/challenge_11.c
 
 install -d ./ctf_ucon2/admin/interface/telnetd
+install -d ./ctf_ucon2/admin/interface/web
 cp -af interface/telnetd/telnetd.rb ./ctf_ucon2/admin/interface/telnetd/
 cp -af interface/telnetd/score.erb ./ctf_ucon2/admin/interface/telnetd/
-
-install -d ./ctf_ucon2/admin/interface/web
+cp -af interface/web/ucon2 ./ctf_ucon2/admin/interface/web/
 
 install -d ./ctf_ucon2/bot
 cp -af ./../bot/counter_bot* ./ctf_ucon2/bot/
