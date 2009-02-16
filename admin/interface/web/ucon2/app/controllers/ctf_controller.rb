2b75fdafe7bd82474dfb342a6cd6ed79fa9fd7ce
Marcos Alvares <mabj@levinas.(none)>
2009-02-16 19:21:31 -0300

    Adicionando o codigo da interface WEB do Capture The Flag

--- admin/interface/web/ucon2/app/controllers/ctf_controller.rb
+++ admin/interface/web/ucon2/app/controllers/ctf_controller.rb
@@ -1,2 +1,14 @@
 class CtfController < ApplicationController
+
+	def about
+		render :layout => false
+	end
+
+	def log
+		render :layout => false
+	end
+
+	def score
+		render :layout => false
+	end
 end
