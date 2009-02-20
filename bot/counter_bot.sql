6c89bda1307e44ab07cae8704f528dee535acefe
Marcos Alvares <mabj@levinas.(none)>
2009-02-20 12:06:31 -0300

    alterando scripts que faziam uso do banco de dados com o esquema antigo

--- bot/counter_bot.sql
+++ bot/counter_bot.sql
@@ -17,14 +17,14 @@ CREATE TABLE challenges_users (
   challenge_id INTEGER NOT NULL
 );
 
-INSERT into challenges values (NULL, 'challenge 01', '1', '1', 1001);
-INSERT into challenges values (NULL, 'challenge 02', '1', '1', 1002);
-INSERT into challenges values (NULL, 'challenge 03', '1', '2', 1003);
-INSERT into challenges values (NULL, 'challenge 04', '1', '2', 1004);
-INSERT into challenges values (NULL, 'challenge 05', '1', '3', 1005);
-INSERT into challenges values (NULL, 'challenge 06', '1', '1', 1006);
-INSERT into challenges values (NULL, 'challenge 07', '1', '1', 1007);
-INSERT into challenges values (NULL, 'challenge 08', '1', '2', 1008);
-INSERT into challenges values (NULL, 'challenge 09', '1', '2', 1009);
-INSERT into challenges values (NULL, 'challenge 10', '1', '3', 1010);
-INSERT into challenges values (NULL, 'challenge 11', '1', '5', 1011);
+INSERT into challenges values (NULL, 'challenge 01', '1', '1', 1001, '', '');
+INSERT into challenges values (NULL, 'challenge 02', '1', '1', 1002, '', '');
+INSERT into challenges values (NULL, 'challenge 03', '1', '2', 1003, '', '');
+INSERT into challenges values (NULL, 'challenge 04', '1', '2', 1004, '', '');
+INSERT into challenges values (NULL, 'challenge 05', '1', '3', 1005, '', '');
+INSERT into challenges values (NULL, 'challenge 06', '1', '1', 1006, '', '');
+INSERT into challenges values (NULL, 'challenge 07', '1', '1', 1007, '', '');
+INSERT into challenges values (NULL, 'challenge 08', '1', '2', 1008, '', '');
+INSERT into challenges values (NULL, 'challenge 09', '1', '2', 1009, '', '');
+INSERT into challenges values (NULL, 'challenge 10', '1', '3', 1010, '', '');
+INSERT into challenges values (NULL, 'challenge 11', '1', '5', 1011, '', '');
