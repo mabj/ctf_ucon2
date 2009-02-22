e29974b3b2a9936ca2af67cc2a4485f04a7a8050
Marcos Alvares <marcos.alvares@gmail.com>
2009-02-22 16:07:55 -0300

    Tabela de scores

--- admin/interface/web/ucon2/app/controllers/ctf_controller.rb
+++ admin/interface/web/ucon2/app/controllers/ctf_controller.rb
@@ -1,14 +1,19 @@
 class CtfController < ApplicationController
 
-	def about
-		render :layout => false
-	end
+  def index
+    @users = User.find(:all).sort {|a, b| a.score <=> b.score }
+  end
 
-	def log
-		render :layout => false
-	end
+  def about
+    render :layout => false
+  end
 
-	def score
-		render :layout => false
-	end
+  def log
+    render :layout => false
+  end
+
+  def score
+    @users = User.find(:all).sort {|a, b| a.score <=> b.score }
+    render :layout => false
+  end
 end
--- admin/interface/web/ucon2/app/models/user.rb
+++ admin/interface/web/ucon2/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 	has_and_belongs_to_many :challenges
+
+  def score
+    self.challenges.map {|x| x.score }.inject {|a,b| a + b}.to_i
+  end
 end
