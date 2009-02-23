b53ded480943cbbd42b33689be0078d538ff2460
Marcos Alvares <marcos.alvares@gmail.com>
2009-02-23 20:42:30 -0300

    Refatorando script de build do capture the flag

--- generate_environment.sh
+++ generate_environment.sh
@@ -1,35 +1,91 @@
 #!/bin/sh
+INSTALL="/usr/bin/install -d"
+FIND="/usr/bin/find"
+RM="/bin/rm -rf"
+WORKDIR="./ctf_ucon2_build"
+CP="/bin/cp -af"
+ECHO="/bin/echo -e"
+TAR="/bin/tar cfz"
 
-rm -rf ctf_ucon2
+cleanup_environment() {
+  ${ECHO} '[+] Cleanup the environment for a new build ...'
+  ${FIND} ./ -iname *~ -exec ${RM} {} \;
+  ${RM} ${WORKDIR}
+}
 
-install -d ./ctf_ucon2/admin
-cp -af create_user.sh ./ctf_ucon2/admin
-cp -af remove_user.sh ./ctf_ucon2/admin
-cp -af sendEmail-v1.55/ ./ctf_ucon2/admin
-cp -af setup.sh ./ctf_ucon2/admin
+build_admin_directory() {
+  ${INSTALL} ${WORKDIR}/admin
+  ${CP} ./admin/create_user.sh ${WORKDIR}/admin
+  ${CP} ./admin/remove_user.sh ${WORKDIR}/admin
+  ${CP} ./admin/sendEmail-v1.55/ ${WORKDIR}/admin
+  ${CP} ./admin/setup_environment.sh ${WORKDIR}/admin
+}
 
-install -d ./ctf_ucon2/admin/code
-install -d ./ctf_ucon2/admin/code/crackme
-install -d ./ctf_ucon2/admin/code/vulndev
+copy_crackme_sources() {
+  ${ECHO} "[+] coping crackme challenges sources"
+  ${CP} ./challenges/crack/challenge_01.c ${WORKDIR}/admin/code/crackme/challenge_01.c
+  ${CP} ./challenges/crack/challenge_02.c ${WORKDIR}/admin/code/crackme/challenge_02.c
+  ${CP} ./challenges/crack/challenge_03.c ${WORKDIR}/admin/code/crackme/challenge_03.c
+  ${CP} ./challenges/crack/challenge_05.c ${WORKDIR}/admin/code/crackme/challenge_04.c
+  ${CP} ./challenges/crack/challenge_04.c ${WORKDIR}/admin/code/crackme/challenge_05.c
+}
 
-cp -af ../crack/challenge_01.c ./ctf_ucon2/admin/code/crackme/challenge_01.c
-cp -af ../crack/challenge_02.c ./ctf_ucon2/admin/code/crackme/challenge_02.c
-cp -af ../crack/challenge_03.c ./ctf_ucon2/admin/code/crackme/challenge_03.c
-cp -af ../crack/challenge_05.c ./ctf_ucon2/admin/code/crackme/challenge_04.c
-cp -af ../crack/challenge_04.c ./ctf_ucon2/admin/code/crackme/challenge_05.c
+copy_vulndev_sources() {
+  ${ECHO} "[+] Coping vulndev challenges sources"
+  ${CP} ./challenges/stack/challenge_01.c ${WORKDIR}/admin/code/vulndev/challenge_06.c
+  ${CP} ./challenges/format/challenge_01.c ${WORKDIR}/admin/code/vulndev/challenge_07.c
+  ${CP} ./challenges/stack/challenge_04.c ${WORKDIR}/admin/code/vulndev/challenge_08.c
+  ${CP} ./challenges/format/challenge_03.c ${WORKDIR}/admin/code/vulndev/challenge_09.c
+  ${CP} ./challenges/stack/challenge_06.c ${WORKDIR}/admin/code/vulndev/challenge_10.c
+  ${CP} ./challenges/stack/challenge_05.c ${WORKDIR}/admin/code/vulndev/challenge_11.c
+}
 
-cp -af ../stack/challenge_01.c ./ctf_ucon2/admin/code/vulndev/challenge_06.c
-cp -af ../format/challenge_01.c ./ctf_ucon2/admin/code/vulndev/challenge_07.c
-cp -af ../stack/challenge_04.c ./ctf_ucon2/admin/code/vulndev/challenge_08.c
-cp -af ../format/challenge_03.c ./ctf_ucon2/admin/code/vulndev/challenge_09.c
-cp -af ../stack/challenge_06.c ./ctf_ucon2/admin/code/vulndev/challenge_10.c
-cp -af ../stack/challenge_05.c ./ctf_ucon2/admin/code/vulndev/challenge_11.c
+copy_challenges_sources() {
+  ${ECHO} "[+] Creating challenges directories"
+  ${INSTALL} ${WORKDIR}/admin/code
+  ${INSTALL} ${WORKDIR}/admin/code/crackme
+  ${INSTALL} ${WORKDIR}/admin/code/vulndev
+  copy_crackme_sources
+  copy_vulndev_sources
+}
 
-install -d ./ctf_ucon2/interface/telnetd
-install -d ./ctf_ucon2/interface/web
-cp -af interface/telnetd/* ./ctf_ucon2/interface/telnetd/
-cp -af interface/web/ucon2/* ./ctf_ucon2/interface/web/
+build_interface_directory() {
+  ${ECHO} "[+] Build interface directory"
+  ${INSTALL} ${WORKDIR}/interface/telnetd
+  ${INSTALL} ${WORKDIR}/interface/web
+  ${CP} interface/telnetd/* ${WORKDIR}/interface/telnetd/
+  ${CP} interface/web/ucon2/* ${WORKDIR}/interface/web/
+}
 
-install -d ./ctf_ucon2/bot
-cp -af ./../bot/counter_bot* ./ctf_ucon2/bot/
+build_bot_directory() {
+  ${ECHO} "[+] Build bot directory"
+  ${INSTALL} ${WORKDIR}/bot
+  ${CP} ./bot/counter_bot* ${WORKDIR}/bot/
+}
 
+compress_build_directory() {
+  ${ECHO} "[+] Compressing build directory"
+  ${TAR} ${WORKDIR}.tar.bz2 ${WORKDIR}
+}
+
+cleanup_tmp_build_directory() {
+  ${ECHO} "[+] Cleanup temp build directory"
+  ${RM} ctf_ucon2_build
+}
+
+print_finish_message() {
+  ${ECHO}
+  ${ECHO} ".:: The Capture the Flag enviroment was created with success"
+  ${ECHO} ".:: Copy the ${WORKDIR}.tar.bz2 to the virtual machine"
+  ${ECHO}
+}
+
+cleanup_environment
+build_admin_directory
+copy_challenges_sources
+build_interface_directory
+build_bot_directory
+compress_build_directory
+cleanup_tmp_build_directory
+print_finish_message
+${ECHO} "DONE ! \O/"
