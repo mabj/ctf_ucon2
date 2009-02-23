13b7959646aa8236297b26dc5d5c187b6a41cdee
Marcos Alvares <marcos.alvares@gmail.com>
2009-02-23 13:57:46 -0300

    Alteranddo os scripts de migration para adicionar os registros dos desafios diretamente no banco

--- admin/interface/web/ucon2/db/migrate/20090217171955_create_challenges.rb
+++ admin/interface/web/ucon2/db/migrate/20090217171955_create_challenges.rb
@@ -7,17 +7,19 @@ class CreateChallenges < ActiveRecord::Migration
       t.integer :uid
       t.timestamps
     end
-    Challenge.create :id => 1001, :name => "Challenge 01", :level => 1, :score => 1, :uid => 1001
-    Challenge.create :id => 1002, :name => "Challenge 02", :level => 1, :score => 1, :uid => 1002
-    Challenge.create :id => 1003, :name => "Challenge 03", :level => 2, :score => 2, :uid => 1003
-    Challenge.create :id => 1004, :name => "Challenge 04", :level => 2, :score => 2, :uid => 1004
-    Challenge.create :id => 1005, :name => "Challenge 05", :level => 3, :score => 3, :uid => 1005
-    Challenge.create :id => 1006, :name => "Challenge 06", :level => 1, :score => 1, :uid => 1006
-    Challenge.create :id => 1007, :name => "Challenge 07", :level => 1, :score => 1, :uid => 1007
-    Challenge.create :id => 1008, :name => "Challenge 08", :level => 2, :score => 2, :uid => 1008
-    Challenge.create :id => 1009, :name => "Challenge 09", :level => 2, :score => 2, :uid => 1009
-    Challenge.create :id => 1010, :name => "Challenge 10", :level => 3, :score => 3, :uid => 1010
-    Challenge.create :id => 1011, :name => "Challenge 11", :level => 3, :score => 5, :uid => 1011
+
+    Challenge.connection.execute("INSERT into challenges values ('1001', 'challenge 01', '1', '1', 1001, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1002', 'challenge 02', '1', '1', 1002, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1003', 'challenge 03', '1', '2', 1003, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1004', 'challenge 04', '1', '2', 1004, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1005', 'challenge 05', '1', '3', 1005, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1006', 'challenge 06', '1', '1', 1006, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1007', 'challenge 07', '1', '1', 1007, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1008', 'challenge 08', '1', '2', 1008, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1009', 'challenge 09', '1', '2', 1009, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1010', 'challenge 10', '1', '3', 1010, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+    Challenge.connection.execute("INSERT into challenges values ('1011', 'challenge 11', '1', '5', 1011, '2009-01-01 01:01:01', '2009-01-01 01:01:01')")
+
   end
 
   def self.down
