4107210b8c5b83dd1bd80574d7eed94994bb14a9
Marcos Alvares <marcos.alvares@gmail.com>
2009-02-23 21:15:09 -0300

    Refatorando codigo do script de remover um usuario do sitema

--- admin/remove_user.sh
+++ admin/remove_user.sh
@@ -5,10 +5,38 @@ ECHO="/bin/echo -e"
 USERDEL="/usr/sbin/userdel"
 CHATTR="/usr/bin/chattr"
 
+remove_user_home_attributes() {
+  ${ECHO} "[+] Removing file attributes..."
+
+  for i in $(seq 1 5); do
+    ${CHATTR} -i "/home/${1}/ucon2/crackme/challenge_0${i}/challenge_0${i}"
+    ${CHATTR} -a "/home/${1}/ucon2/crackme/challenge_0${i}/challenge_0${i}.tag"
+  done
+
+  for i in $(seq -w 6 11); do
+    ${CHATTR} -i  "/home/${1}/ucon2/vulndev/challenge_${i}/challenge_${i}"
+    ${CHATTR} -a  "/home/${1}/ucon2/vulndev/challenge_${i}/challenge_${i}.tag"
+  done
+}
+
+remove_user_home_directory() {
+  ${ECHO} "[+] Removing users' home directory..."
+  ${RM} "/home/${1}"
+}
+
+remove_user() {
+  remove_user_home_directory $1
+  ${ECHO} "[+] Removing user registry..."
+  ${USERDEL} ${1}
+}
+
+###################### [MAIN CODE]
+
+# checking parameters and environment before delete the user
 if [ $# != 1 ]; then
-	echo -e ".:: uCon 2 Capture The Flag ::.\n"
-	echo -e "   Usage: ${0} [user_name]\n"
-	exit 1
+  ${ECHO} ".:: uCon 2 Capture The Flag ::.\n"
+  ${ECHO} "   Usage: ${0} [user_name]\n"
+  exit 1
 fi
 
 if [ ! -e "/home/${1}" ]; then
@@ -22,19 +50,7 @@ if [ "${WHOAMI}" != "root" ]; then
   exit 1
 fi
 
-${ECHO} "[+] Removing file attributes..."
+remove_user_home_attributes $1
+remove_user $1
 
-for i in $(seq 1 5); do
-  ${CHATTR} -i "/home/${1}/ucon2/crackme/challenge_0${i}/challenge_0${i}"
-  ${CHATTR} -a "/home/${1}/ucon2/crackme/challenge_0${i}/challenge_0${i}.tag"
-done
-
-for i in $(seq -w 6 11); do
-  ${CHATTR} -i 	"/home/${1}/ucon2/vulndev/challenge_${i}/challenge_${i}"
-  ${CHATTR} -a 	"/home/${1}/ucon2/vulndev/challenge_${i}/challenge_${i}.tag"
-done
-
-${ECHO} "[+] Removing users' home directory..."
-${RM} "/home/${1}"
-${ECHO} "[+] Removing user registry..."
-${USERDEL} ${1}
+${ECHO} "DONE ! \O/"
