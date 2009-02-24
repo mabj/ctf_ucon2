b97b691eaf79ef933c16f8e9536e36db839ef80a
Marcos Alvares <marcos.alvares@gmail.com>
2009-02-23 23:40:28 -0300

    Adicionando o software a licensa GPLv3

--- challenges/stack/challenge_06.c
+++ challenges/stack/challenge_06.c
@@ -1,4 +1,22 @@
 /*
+*  challenge_06.c is a piece of uCon 2009 Capture The Flag code
+*  Copyright (C) 2002 Marcos Alvares <marcos.alvares@gmail.com>
+*
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 3, or (at your option)
+*  any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program; if not, write to the Free Software
+*  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*
 * uCon Security Conference II - Recife / Pernambuco / Brazil - Feb 2009
 *        Challenge 06 - Stack - Difficulty level 03
 *        Author: Gustavo Pimentel <gusbit gmail>
--- interface/telnetd/telnetd.rb
+++ interface/telnetd/telnetd.rb
@@ -1,3 +1,22 @@
+#!/usr/bin/ruby
+#
+# telnetd.rb is a piece of uCon 2009 Capture The Flag code
+# Copyright (C) 2002 Marcos Alvares <marcos.alvares@gmail.com>
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
 ################################################################################
 # Ucon Security Conference II - Capture The Flag - Recife Brazil 2009
 # Author:       Marcos Alvares marcos.alvares at gmail.com
@@ -5,7 +24,6 @@
 #               tempo real para os competidores. Ele faz uso da base sqlite
 #               gerada e alimentada pelo bot
 
-#!/usr/bin/ruby
 require 'socket'
 require 'logger'
 require 'erb'
